69c459e324904ca5a7266fc55b558f844a646df0
Gustav Lindblom <lindblom.gustav@gmail.com>
2016-12-22 15:08:38 +0100

    Woop

---
+++ src/Fishworks.MonoUI/UI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fishworks.MonoUI
+{
+  public class UI
+  {
+    public UI()
+    {
+
+    }
+  }
+}
